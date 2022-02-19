57bff7380af4a8f920c716c7ee4e14f75e0c7bfb
Ricardo <rnavarrete_cervantes@hotmail.com>
2022-02-19 12:40:02 -0600

    Actualizacion última Versión

--- js/index.js
+++ js/index.js
@@ -9,11 +9,15 @@ function validarDatos() {
 
     // let usuario = new x();
     let inputValuesX = document.getElementsByClassName("text-usuario");
-    //let password = new y();
+    console.log(inputValuesX)
+        //let password = new y();
     let inputValuesY = document.getElementsByClassName("pwd-usuario");
+    console.log(inputValuesY)
 
+    nameValue = [{ user: 'Admin', password: 'Admin1234' }, { user: 'Customer', password: 'Customer1234' }].filter(element => element.user === inputValuesX[0].value && element.password === inputValuesY[0].value);
+    console.log(inputValuesX.value)
+    console.log(inputValuesY.value)
 
-    nameValue = [{ user: 'Admin', password: 'Admin1234' }, { user: 'Customer', password: 'Customer1234' }].filter(element => element.user === inputValuesX && element.password === inputValuesY);
     if (nameValue.length > 0) {
         location.href = "panel-admin.html"
     } else {
